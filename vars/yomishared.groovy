b3876d1bc05b25501a61615287df21a9eb9ac0f1
YomiPounds <53308339+YomiPounds@users.noreply.github.com>
2023-01-22 21:41:01 -0500

    Update yomishared.groovy

--- vars/yomishared.groovy
+++ vars/yomishared.groovy
@@ -1,3 +1,3 @@
 def call(String name,String dayOfWeek){
-  echo "my name is ${name} and I am a devops engineer and today is ${dayOf Week}"
+  echo "my name is ${name} and I am a devops engineer and today is ${dayOfWeek}"
 }
